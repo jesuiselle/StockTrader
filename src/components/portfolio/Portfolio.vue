ccf629a3cf4d48ed17e0f05f69d278eeebf4a0d4
Oumayma Dhehibi <oumayma.dhehibi@esprit.tn>
2020-06-17 15:31:35 +0200

    first commit

---
+++ src/components/portfolio/Portfolio.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <app-stock-portfolio v-for="stock in stocks" :stock="stock"></app-stock-portfolio>
+    </div>
+</template>
+
+<script>
+    import {mapGetters} from 'vuex';
+    import Stock from '../portfolio/Stock.vue';
+
+    export default {
+        computed: {
+            ...mapGetters({
+                stocks: 'stockPortfolio'
+            })
+        },
+        components: {
+            appStockPortfolio: Stock
+        }
+    }
+</script>
---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueRouter from "vue-router";
+import App from './App.vue';
+import { routes } from './routes';
+import VueResource from 'vue-resource';
+import store from "./store/store";
+
+Vue.use(VueRouter);
+Vue.use(VueResource)
+
+Vue.http.options.root = 'https://vuejs-stock-trader-8cc49.firebaseio.com/';
+
+Vue.filter('currency', (value) => {
+  return '$' + value.toLocaleString();
+});
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
